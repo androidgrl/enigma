4a9406c6cfbb3d7e9e6a49082c00dec2755948dd
androidgrl <kawaharajamie@gmail.com>
2015-03-03 20:15:29 -0700

    adding offset class

--- lib/offset.rb
+++ lib/offset.rb
@@ -1,7 +1,16 @@
 class Offset
   attr_reader :date
 
-  def initialize
-    @date = Time.now.strftime("%M%D%Y")
+  def initialize(date=Time.now.strftime("%m%m%y"))
+    @date = date
+  end
+
+  def integer_date
+    date.to_i
+  end
+
+  def create_offset
+    digits = (integer_date ** 2).to_s.chars
+    digits[-4..-1].map{|d| d.to_i }
   end
 end
--- test/offset_test.rb
+++ test/offset_test.rb
@@ -9,7 +9,48 @@ class TestOffset < Minitest::Test
     assert Offset
   end
 
-  def test_it_has_a_date
-    assert
+  def test_it_has_a_date_method
+    offset = Offset.new
+    assert offset.respond_to?(:date)
+  end
+
+  def test_it_can_create_an_integer_date_from_a_date_not_staring_with_zero
+    offset = Offset.new("120315")
+
+    integer_date = offset.integer_date
+
+    assert_equal 120315, integer_date
+  end
+
+  def test_it_can_create_an_integer_date_from_a_date_starting_with_zero
+    offset = Offset.new("030315")
+
+    integer_date = offset.integer_date
+
+    assert_equal 30315, integer_date
+  end
+
+  def test_it_can_create_an_offset_from_a_date_starting_with_zero
+    offset = Offset.new("022677")
+
+    offset = offset.create_offset
+
+    assert_equal [6, 3, 2, 9], offset
+  end
+
+  def test_it_can_create_an_offset_from_a_date_ending_with_zero
+    offset = Offset.new("122600")
+
+    offset = offset.create_offset
+
+    assert_equal [0, 0, 0, 0], offset
+  end
+
+  def test_it_can_create_an_offset_from_a_date_neither_starting_nor_ending_with_zero
+    offset = Offset.new("122601")
+    
+    offset = offset.create_offset
+
+    assert_equal [5, 2, 0, 1], offset
   end
 end
